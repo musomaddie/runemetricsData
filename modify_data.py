9ef1638dc3f83e0952a1794b644d374929601e2a
Maddie Wagner <musomaddy@gmail.com>
2021-09-08 20:49:31 +1000

    Completed allowing merge of data

--- modify_data.py
+++ modify_data.py
@@ -1,6 +1,8 @@
 from util import TextFormat
 from util import confirm_input
+from util import make_filename
 from util import read_data_from_csv
+from util import write_data_to_csv
 
 _TESTING = True
 
@@ -21,15 +23,76 @@ def _scroll(data, data_name):
         # commands)
 
 
-def _merge(data, data_name):
-    pass
-    # Allow the user to enter the name of item 1
+def _calc_index(df, matching_value, col_name):
+    """
+    Returns -1 if such a row is not found, otherwise returns the appriorate
+        index.
+    """
+    try:
+        return df.index[df[col_name] == matching_value].item()
+    except ValueError:
+        raise ValueError()
 
-    # Allow the user to enter the name of second item
 
-    # Allow the user to select the name to keep
+def _fetch_row_from_dataframe_given_name(df, name, col_name):
+    """ Returns a row from the dataframe that matches the specific name given.
+    """
+    try:
+        return df.iloc[_calc_index(df, name, col_name)]
+    except ValueError:
+        return None
 
-    # Do the merge
+
+def _merge_dates_dict(dates1, dates2):
+    for date in dates2:
+        if date in dates1:
+            dates1[date] += dates2[date]
+        else:
+            dates1[date] = dates2[date]
+
+
+def _merge_items(df, col_name):
+    # NOTE: this keeps all the data from the first item selected not the second
+    # Making the deliberate choice to keep this CASE SENSITIVE to allow merging
+    # when the only difference is case
+    option_list = df[col_name].tolist()
+    item1_name = confirm_input(
+        input("What is the first item you would like to merge? "),
+        option_list + ["cancel"],
+        case_sensitive=True)
+
+    if item1_name == "cancel":
+        return
+    option_list.remove(item1_name)
+
+    item2_name = confirm_input(
+        input("What is the second item you would like to merge? "),
+        option_list + ["cancel"],
+        case_sensitive=True)
+    if item2_name == "cancel":
+        return
+
+    item1 = _fetch_row_from_dataframe_given_name(df, item1_name, col_name)
+    item2 = _fetch_row_from_dataframe_given_name(df, item2_name, col_name)
+
+    if item1 is None or item2 is None:
+        print(f"One of the items {TextFormat.RED}does not exist"
+              f"{TextFormat.END}. Please select a different option or"
+              " try again.")
+        return
+
+    updated_name = input(f"What is the name of the merged item (current names"
+                         f" are {TextFormat.GREEN}{item1_name}{TextFormat.END}"
+                         f" and {TextFormat.GREEN}{item2_name}{TextFormat.END}"
+                         "? ")
+    # TODO: if there are any differences between the remaining columns question
+    # those
+
+    _merge_dates_dict(item1["dates"], item2["dates"])
+    df = df.drop(index=_calc_index(df, item2_name, col_name))
+    df.at[_calc_index(df, item1_name, col_name), col_name] = updated_name
+
+    write_data_to_csv(make_filename(_TESTING, f"{col_name}s"), df)
 
 
 def _sort_and_print(data, data_name):
@@ -40,50 +103,39 @@ def _sort_and_print(data, data_name):
     print()
 
 
-def combine():
-    """
+def combine_items():
     data_name = confirm_input(
         input(f"Combining {TextFormat.GREEN}kills{TextFormat.END} or "
               f"{TextFormat.GREEN}drops{TextFormat.END}? "),
         ["kills", "drops"])
-    """
-    data_name = "drops"
-    data = read_data_from_csv(_TESTING, data_name)
+    data = read_data_from_csv(make_filename(_TESTING, data_name))
     data_name = data_name[:-1]
 
     _sort_and_print(data, data_name)
 
     while True:
-        """
         user_input = confirm_input(
             input(f"What would you like to do now ({TextFormat.GREEN}merge"
                   f"{TextFormat.END}, {TextFormat.GREEN}scroll{TextFormat.END}"
                   f", or {TextFormat.GREEN}quit{TextFormat.END})? "),
             ["merge", "scroll", "quit"])
-        """
-        user_input = "merge"
         if user_input == "quit":
             break
         if user_input == "scroll":
             _scroll(data, data_name)
         if user_input == "merge":
-            _merge(data, data_name)
+            _merge_items(data, data_name)
         break
 
 
-    # Update the csv
-
-
 def start():
-    """
     opt = confirm_input(
         input(f"Are you wanting to {TextFormat.GREEN} combine"
               f"{TextFormat.END} or ... ? "),
         ["combine"])
-    """
     opt = "combine"
     if opt == "combine":
-        combine()
+        combine_items()
 
 
 if __name__ == "__main__":
